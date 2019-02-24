c8707e848ee12f7b151943af41b05c1e2fe7b8d6
orbb1 <orbbmax@gmail.com>
2019-02-24 17:56:08 +0100

    eating apple, random apple position.

--- src/snake.js
+++ src/snake.js
@@ -11,16 +11,25 @@ const getInitialState = () => ({
   move: [DOWN],
 });
 
+const getRandomPos = () => ({
+  x: Math.floor(Math.random() * getInitialState().cols) + 0,
+  y: Math.floor(Math.random() * getInitialState().rows) + 0,
+});
+const checkIfEqual = p1 => p2 => p1.x === p2.x && p1.y === p2.y;
 const dropLast = snakeArr => snakeArr.slice(0, snakeArr.length - 1);
 const getProp = key => obj => obj[key];
 const move = x => y => ((y % x) + x) % x;
 const nextHead = state => state.snake.length < 1
-  ? { x: 1, y: 6 }
+  ? { x: 10, y: 2 }
   : {
     x: move(state.cols)(state.snake[0].x + state.move[0].x),
     y: move(state.rows)(state.snake[0].y + state.move[0].y),
   };
-const nextSnake = state => [nextHead(state)].concat(dropLast(state.snake));
+const willEat = state => checkIfEqual(nextHead(state))(state.apple);
+const nextApple = state => willEat(state) ? getRandomPos(state) : state.apple;
+const nextSnake = state => willEat(state)
+  ? [nextHead(state)].concat(state.snake)
+  : [nextHead(state)].concat(dropLast(state.snake));
 const makeNextState = newState => oldState => Object.keys(newState)
   .map(key => ({ [key]: newState[key](oldState) }))
   .reduce((acc, obj) => Object.assign(acc, obj), {});
@@ -28,7 +37,7 @@ const makeNextState = newState => oldState => Object.keys(newState)
 const nextState = makeNextState({
   rows: getProp('rows'),
   cols: getProp('cols'),
-  apple: () => ({ x: 10, y: 7 }),
+  apple: nextApple,
   snake: nextSnake,
   move: () => [DOWN],
 });
