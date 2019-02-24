76e2a4a639a0970c11cbcd7a4049f6e9fc3cf649
orbb1 <orbbmax@gmail.com>
2019-02-24 18:31:14 +0100

    key bindings, crash.

--- src/console-grid.js
+++ src/console-grid.js
@@ -1,4 +1,7 @@
-import { getInitialState, nextState } from './snake';
+import * as readline from 'readline';
+import {
+  getInitialState, nextState, addMove, vectors,
+} from './snake';
 import {
   repeat, pipe, adjust, k,
 } from './helpers';
@@ -20,7 +23,20 @@ const Grid = {
   )(state),
 };
 
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+  if (key.ctrl && key.name === 'c') process.exit();
+  switch (key.name.toUpperCase()) {
+    case 'W': case 'UP': State = addMove(State, vectors.UP); break;
+    case 'A': case 'LEFT': State = addMove(State, vectors.LEFT); break;
+    case 'S': case 'DOWN': State = addMove(State, vectors.DOWN); break;
+    case 'D': case 'RIGHT': State = addMove(State, vectors.RIGHT); break;
+    default: console.log('press WASD or arrows');
+  }
+});
+
 const show = () => console.log(`\x1Bc ${Grid.toString(Grid.fromState(State))}`);
 const step = () => { State = nextState(State); };
 
-setInterval(() => { step(); show(); }, 500);
+setInterval(() => { step(); show(); }, 200);
--- src/snake.js
+++ src/snake.js
@@ -1,14 +1,16 @@
-const UP = { x: 0, y: -1 };
-const DOWN = { x: 0, y: 1 };
-const RIGHT = { x: 1, y: 0 };
-const LEFT = { x: -1, y: 0 };
+const vectors = {
+  UP: { x: 0, y: -1 },
+  DOWN: { x: 0, y: 1 },
+  RIGHT: { x: 1, y: 0 },
+  LEFT: { x: -1, y: 0 },
+};
 
 const getInitialState = () => ({
   cols: 20,
   rows: 15,
   apple: { x: 10, y: 7 },
   snake: [],
-  move: [DOWN],
+  move: [vectors.DOWN],
 });
 
 const getRandomPos = () => ({
@@ -16,7 +18,15 @@ const getRandomPos = () => ({
   y: Math.floor(Math.random() * getInitialState().rows) + 0,
 });
 const checkIfEqual = p1 => p2 => p1.x === p2.x && p1.y === p2.y;
+const checkIfValidMove = move => state => (
+  state.move[0].x + move.x !== 0 || state.move[0].y + move.y !== 0
+);
+
+const addMove = (state, vector) => checkIfValidMove(vector)(state)
+  ? Object.assign({}, state, { move: state.move.concat([vector]) })
+  : state;
 const dropLast = snakeArr => snakeArr.slice(0, snakeArr.length - 1);
+const dropFirst = snakeArr => snakeArr.slice(1);
 const getProp = key => obj => obj[key];
 const move = x => y => ((y % x) + x) % x;
 const nextHead = state => state.snake.length < 1
@@ -25,11 +35,16 @@ const nextHead = state => state.snake.length < 1
     x: move(state.cols)(state.snake[0].x + state.move[0].x),
     y: move(state.rows)(state.snake[0].y + state.move[0].y),
   };
+const willCrash = state => state.snake.find(checkIfEqual(nextHead(state)));
 const willEat = state => checkIfEqual(nextHead(state))(state.apple);
 const nextApple = state => willEat(state) ? getRandomPos(state) : state.apple;
-const nextSnake = state => willEat(state)
+const willLive = state => willEat(state)
   ? [nextHead(state)].concat(state.snake)
   : [nextHead(state)].concat(dropLast(state.snake));
+const nextSnake = state => willCrash(state)
+  ? []
+  : willLive(state);
+const nextMoves = state => state.move.length > 1 ? dropFirst(state.move) : state.move;
 const makeNextState = newState => oldState => Object.keys(newState)
   .map(key => ({ [key]: newState[key](oldState) }))
   .reduce((acc, obj) => Object.assign(acc, obj), {});
@@ -39,7 +54,9 @@ const nextState = makeNextState({
   cols: getProp('cols'),
   apple: nextApple,
   snake: nextSnake,
-  move: () => [DOWN],
+  move: nextMoves,
 });
 
-export { getInitialState, nextState };
+export {
+  getInitialState, nextState, addMove, vectors,
+};
